d12f1419b4a7a5e8363658b3497eef2ac1950a41
spmaxsp <mexx@spahn-rodgau.de>
2022-12-10 12:44:58 +0100

    fixing api for complex numbers with \angle

--- pycalc/calc.py
+++ pycalc/calc.py
@@ -1,10 +1,18 @@
 from latex2sympy2 import latex2latex
 
+from latex2sympy2 import set_real
+
+set_real(False)
+
 def do_calculation(argument):
-    argument = argument.replace('\differentialD', 'd')
-    argument = argument.replace('\mathop{=}', '=')
+    argument = argument.replace('\\differentialD', 'd')
+    argument = argument.replace('\\mathop{=}', '=')
+    argument = argument.replace('\\angle', '(e^i)^')
+    argument = argument.replace('\\left(', '{(')
+    argument = argument.replace(')\\right', ')}')
     
-    result = latex2latex(argument)
+    print(argument)
+    result = latex2latex(argument,)
     print(result)
     return  result# for now just return the argument
 
